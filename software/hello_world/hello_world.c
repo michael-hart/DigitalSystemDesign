7dfd2dd63d4394e2285b0e8ba073acc2aa577780
Michael Hart <michael.hart1994@gmail.com>
2016-01-19 12:38:54 +0000

    Modified hello world file to contain code given in instructions

--- software/hello_world/hello_world.c
+++ software/hello_world/hello_world.c
@@ -1,24 +1,71 @@
 /*
- * "Hello World" example.
+ * Modified "Hello, world" template for Nios II, intended to compute a mathematical function on a vector.
  *
- * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
- * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example
- * designs. It runs with or without the MicroC/OS-II RTOS and requires a STDOUT
- * device in your system's hardware.
- * The memory footprint of this hosted application is ~69 kbytes by default
- * using the standard reference design.
- *
- * For a reduced footprint version of this template, and an explanation of how
- * to reduce the memory footprint for a given application, see the
- * "small_hello_world" template.
+ * Produces a vector based on defined constants and performs custom code on that vector.
+ * Also times the custom code and outputs to stdout.
  *
  */
 
-#include <stdio.h>
+#include <stdlib.h>
+#include <sys/alt_stdio.h>
+#include <sys/alt_alarm.h>
+#include <sys/times.h>
+#include <alt_types.h>
+#include <system.h>
+#include <unistd.h>
+
+// Test case 1
+//#define step 5
+//#define N 52
+
+// Test case 2
+#define step 0.1
+#define N 2551
+
+//Test case 3
+//#define step 0.001
+//#define N 255001
+
+
+// Generates the vector x and stores it in memory
+void generateVector(float x[N])
+{
+	int i;
+	x[0] = 0;
+	for (i=0; i<N; i++)
+	{
+		x[i] = x[i-1] + step;
+	}
+}
+
+float sumVector(float x[], int M)
+{
+	// YOUR CODE GOES HERE
+}
 
 int main()
 {
-  printf("Hello from Nios II!\n");
+	printf("Task 2!\n");
 
-  return 0;
+	// Define input vector
+	float x[N];
+
+	// Returned result
+	float y;
+
+	generateVector(x);
+
+	// The following is used for timing
+	char buf[50];
+	clock_t exec_t1, exec_t2;
+
+	exec_t1 = times(NULL); // get system time before starting the process
+
+// The code that you want to time goes here
+	y = sumVector(x, N);
+
+	// till here
+	exec_t2 = times(NULL); // get system time after finishing the process
+
+	gcvt((exec_t2 - exec_t1), 10, buf);
 }
